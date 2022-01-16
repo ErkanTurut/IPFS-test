f623de07f90eb14bd83ca16ff5b92d11790c0a1d
SirKamso <erkamturut@gmail.com>
2022-01-16 17:57:35 +0100

    installation + test ipfs

---
+++ main.js
@@ -0,0 +1,61 @@
+import { create } from "ipfs-http-client";
+
+async function ipfsClient() {
+  //const client = create("http://localhost:80");
+
+  const ipfs = await create({
+    host: "ipfs.infura.io",
+    port: 5001,
+    protocol: "https",
+  });
+
+  return ipfs;
+}
+
+async function addText() {
+  let ipfs = await ipfsClient();
+  //const t = "Erkam";
+  let response = await ipfs.add(`La grosse daronne à Benjamin.`);
+  console.log(response);
+}
+
+addText();
+
+async function addFile() {
+  let ipfs = await ipfsClient();
+
+  let data = {
+    name: "Turut",
+    age: "20",
+    sex: "M",
+  };
+
+  let options = {
+    warpWithDirectory: true,
+    progress: (prog) => console.log(`Saved : ${prog}`),
+  };
+
+  let response = await ipfs.add(
+    {
+      path: "test.json",
+      content: JSON.stringify(data),
+    },
+    options
+  );
+  console.log(response);
+}
+
+//addFile();
+
+async function getData(hash) {
+  let ipfs = await ipfsClient();
+
+  let response = ipfs.cat(hash);
+
+  for await (const itr of response) {
+    let data = Buffer.from(itr).toString();
+    console.log(data);
+  }
+}
+
+getData("QmSytc9R1af69PEdm9fUxCv4qZPpHP23xes7V6BWqi6GAd");
